523e1a099b284b6de74bd0bda014f7156d75e739
chenlianbing <lianbch@gmail.com>
2014-06-21 07:29:17 +0800

    Phrase III: install xlrd module, and change the usage of module, use modulename.objectname

--- excelfile_proc.py
+++ excelfile_proc.py
@@ -1,9 +1,13 @@
 # -*- coding: UTF-8 -*-
 import csv
+import xlrd
 
-DBG_ConfigProductList = ['田园成品窗帘 隔热全遮光布 客厅卧室定制遮阳窗帘布特价清仓星星',\
+DBG_ConfigProductTitle = ['田园成品窗帘 隔热全遮光布 客厅卧室定制遮阳窗帘布特价清仓星星',\
 						 '窗帘遮光布料成品宜家田园卧室客厅定制飘窗帘布窗纱特价清仓波点',\
 						 '欧式成品清仓 定制拼接半遮光窗帘高档客厅卧室提花窗帘布料特价']
+DBG_ConfigProductName = ['星星',\
+						 '左右爱',\
+						 '瓶安锦绣']						 
 DBG_ConfigProductData = ['', '', '']
 
 
@@ -12,13 +16,22 @@ DBG_ConfigProductData = ['', '', '']
 def findInList(list, value):
 	try:
 		pos = -1
-		for i in range(0, len(list):
+		for i in range(0, len(list)):
 			if (list[i] == value):
 				pos = i
 				break
 		return pos		
 	except:
-		print "findInList Exception!!!"
+		print ("findInList Exception!!!")
+
+# Get the product name according to the title		
+def getProductName(title):
+	try:
+		pos = findInList(DBG_ConfigProductTitle, title)
+		if (pos != -1):
+			return DBG_ConfigProductName[pos]
+	except:
+		print ("getProductName: Invalid Product Title!!!")
 
 # Get product's basic data from line which read from the csv file
 # and composite into new record
@@ -27,19 +40,28 @@ def findInList(list, value):
 # line[8] : order
 # format: rank: 星星@13074.28@114@1/5/2@-1
 def getProductData(line):
-	productName = getProductName()
+	productName = getProductName(line[2])
+	productSale = line[6]
+	productOrder = line[8]
+	
+	return ("rank: " + productName + "@" + productSale + "@" + productOrder + "@")
 	
 
 # Process the csv file, and format the product data, all the formated
 # product date store in the globle variable 'DBG_ConfigProductData'
-def DRG_ProcExcelFile(filename):
+def DRG_GetProductInfo(filename):
 	print (filename)
 	with open(filename, newline='') as f:
 		reader = csv.reader(f)
 		
 		for line in reader:
-			if ((pos = findInList(DBG_ConfigProductList, line[2])) != -1):
+			pos = findInList(DBG_ConfigProductTitle, line[2])
+			if (pos != -1):
 				productInfo = getProductData(line)
 				DBG_ConfigProductData[pos] = productInfo			
 		
 	return True
+	
+# Process the csv file, and get rank info from EXCEL file
+def DRG_GetRankInfo(filename, productList):
+	pass
--- main.py
+++ main.py
@@ -2,8 +2,11 @@
 
 import os
 import sys
-from textline_proc import DRG_GetOutputLine, DRG_ParseLine
-from excelfile_proc import DRG_ProcExcelFile
+import textline_proc
+import excelfile_proc
+
+#from textline_proc import textline_proc.DRG_GetOutputLine, DRG_ParseLine
+#from excelfile_proc import DRG_GetProductInfo, DRG_GetRankInfo, DBG_ConfigProductData
 
 DRG_CURR_PATH = os.path.abspath(os.path.dirname(sys.argv[0]))
 DRG_TEST_PATH = DRG_CURR_PATH + "\Test"
@@ -12,17 +15,25 @@ print (DRG_TEST_PATH)
 os.chdir(DRG_TEST_PATH)
 
 # Stage I: Read data from EXCEL file, and write to input_data.txt
-procRes = DRG_ProcExcelFile("baobei-20140618.csv")
+procRes = excelfile_proc.DRG_GetProductInfo("baobei-20140618.csv")
 if (procRes == False):
 	print ("Err.")
-'''
+else:
+	for item in excelfile_proc.DBG_ConfigProductData:
+		print (item)
+	
+procRes = excelfile_proc.DRG_GetRankInfo("销量排名记录.xls", excelfile_proc.DBG_ConfigProductData)	
+if (procRes == False):
+	print ("Err.")
+	
+	'''
 # Stage II: Parse data from input_data and write to report.txt
 read_file = open("input_data.txt", "r")
 write_file = open("report.txt", "w")
 
 # Read each line and do transforming
 for read_line in read_file:
-	out_line = DRG_ParseLine(read_line)
+	out_line = textline_proc.DRG_ParseLine(read_line)
 	
 	if(out_line != "Invalid Line"):
 		print (out_line)
